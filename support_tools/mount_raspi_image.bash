116b1995fccde42793d79f9cf3e6c291298a09af
sazus <sazuzas@gmail.com>
2013-05-10 21:36:04 +0900

    QtCreator使用時に何回もSDCardImageをmountするコマンド打つのがめんどかったので、 Suport_tools以下にmountさせるスクリプト作成。 以下、bakeqtpi関連 bakeqtpiで、qtbaseのexeampleとqtdeclarativeのexampleとtoolが正常にinstallできない不具合修正。 piユーザーの.bashrcにqt5のbinディレクトリにPATHを通す環境変数の追加。

---
+++ support_tools/mount_raspi_image.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILE_RASPBIAN_IMG="../bakeqtpi/opt/2013-02-09-wheezy-raspbian.img"
+DIR_MOUNT="../bakeqtpi/opt/rasp-pi-rootfs"
+
+if [ -e ${FILE_RASPBIAN_IMG} ]; then
+	sudo mount -o loop,offset=62914560 ${FILE_RASPBIAN_IMG} ${DIR_MOUNT}
+fi
+exit
